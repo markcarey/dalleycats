eb4c00331e6ef6ad9f6be6da1ed9ba2067392a77
markcarey <mark@markcarey.com>
2022-12-07 22:56:30 -0500

    support for URI storage

--- contracts/CrossChainCats.sol
+++ contracts/CrossChainCats.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.2;
 
 //import "hardhat-deploy/solc_0.8/proxy/Proxied.sol";
 import "@layerzerolabs/solidity-examples/contracts/contracts-upgradable/token/ONFT721/ONFT721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
 
-contract CrossChainCats is Initializable, ONFT721Upgradeable {
+contract CrossChainCats is Initializable, ONFT721Upgradeable, ERC721URIStorageUpgradeable {
     uint public nextMintId;
     uint public maxMintId;
 
@@ -22,14 +23,40 @@ contract CrossChainCats is Initializable, ONFT721Upgradeable {
 
     function __ONFT721UpgradeableMock_init_unchained(string memory _name, string memory _symbol, address _lzEndpoint) internal onlyInitializing {}
 
-    function mint() external payable {
+    function mint(string memory uri) external payable {
         require(nextMintId <= maxMintId, "UniversalONFT721: max mint limit reached");
 
-        uint newId = nextMintId;
+        uint tokenId = nextMintId;
         nextMintId++;
 
-        _safeMint(msg.sender, newId);
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, uri);
     }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721Upgradeable, ERC721URIStorageUpgradeable) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721Upgradeable, ONFT721Upgradeable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
 
     /**
      * @dev This empty reserved space is put in place to allow future versions to add new
